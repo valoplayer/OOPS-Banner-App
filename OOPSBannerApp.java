b51ef943122bcb20121d9add71c68a36142ea8d9
valoplayer <namanrathore1507@gmail.com>
2026-02-16 08:43:35 +0530

    feat: implemented UC1 - Print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+/**
+ * OOPSBannerApp - UC1
+ * @author Naman Kumar
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
