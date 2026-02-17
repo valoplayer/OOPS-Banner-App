315625e1e1995f87f2c82dc352a4aca3b42649a8
valoplayer <namanrathore1507@gmail.com>
2026-02-17 22:55:50 +0530

    UC3: Refactored banner using String.join

---
+++ OopsBannerUC3.java
@@ -0,0 +1,26 @@
+public class OopsBannerUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                " *****", "   ", " *****", "   ", " ******", "   ", " *****"));
+
+        System.out.println(String.join("",
+                "*     *", " ", "*     *", "  ", "*     *", " ", "*     *"));
+
+        System.out.println(String.join("",
+                "*     *", " ", "*     *", "  ", "*     *", " ", "*     "));
+
+        System.out.println(String.join("",
+                "*     *", " ", "*     *", "  ", "******", "   ", " *****"));
+
+        System.out.println(String.join("",
+                "*     *", " ", "*     *", "  ", "*", "            ", "*"));
+
+        System.out.println(String.join("",
+                "*     *", " ", "*     *", "  ", "*", "      ", "*     *"));
+
+        System.out.println(String.join("",
+                " *****", "   ", " *****", "   ", "*", "       ", " *****"));
+    }
+}
