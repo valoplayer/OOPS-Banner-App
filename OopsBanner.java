bbef2be7d6b090e8f8efb8eb35c193d292d0d2cb
valoplayer <namanrathore1507@gmail.com>
2026-02-17 22:46:13 +0530

    UC2: ASCII OOPS banner implemented

---
+++ OopsBanner.java
@@ -0,0 +1,14 @@
+public class OopsBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   ******   *****");
+        System.out.println("*     * *     *  *     * *     *");
+        System.out.println("*     * *     *  *     * *     ");
+        System.out.println("*     * *     *  ******   *****");
+        System.out.println("*     * *     *  *            *");
+        System.out.println("*     * *     *  *      *     *");
+        System.out.println(" *****   *****   *       *****");
+
+    }
+}
